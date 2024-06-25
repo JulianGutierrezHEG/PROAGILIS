8733275c4cf3ccf8dab494517fd31004b8cc7c09
Julian Gutierrez <julian.gutierrez@etu.hesge.ch>
2024-06-25 11:54:44 +0200

    layout principal

--- frontend/frontend/src/router/index.js
+++ frontend/frontend/src/router/index.js
@@ -1,10 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainLayout from '../components/MainLayout.vue';
 import HomeView from '../views/HomeView.vue'
+import OtherView from '../views/OtherView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/',name: 'home', component: HomeView}
+    {
+      path: '/',
+      component: MainLayout,
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: HomeView
+        },
+        {
+          path: 'other',
+          name: 'Other',
+          component: OtherView
+        }
+      ]
+    }
   ]
 })
 
---
+++ frontend/frontend/src/views/OtherView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1>other page</h1>
+      <p>Welcome to the  page!</p>
+    </div>
+  </template>
+  
+  <script setup>
+
+  </script>
+  
